2d10f445ac68419f37f962693b7741570a2019a5
Artem Erusov <erusovav@gmail.com>
2025-06-24 18:24:04 +0300

    Configured Dev Server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,19 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "production" | "development"
 
 interface EnvVariables{
   mode: Mode
+  port: number
 }
 
 export default  (env: EnvVariables) => {
+
+const isDev = env.mode === "development"
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
@@ -31,6 +35,11 @@ export default  (env: EnvVariables) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
     },
+    devtool: isDev && "inline-source-map" ,
+    devServer : isDev ? {
+      port: env.port ?? 3000,
+      open:true,
+    } : undefined,
   };
   return config;
 };
