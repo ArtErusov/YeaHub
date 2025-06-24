41b44eb8411df9da776c39d80cb621373b6870e5
Artem Erusov <erusovav@gmail.com>
2025-06-24 19:12:50 +0300

    Installed and configured React

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
-const a: number = 5;
-const b: number = 5;
-console.log(a + b);
-console.log('Хелло');
+import { createRoot } from 'react-dom/client';
+import { App } from './components/App';
+import React from 'react';
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('root not found');
+}
+
+const container = createRoot(root);
+
+container.render(<App />);
