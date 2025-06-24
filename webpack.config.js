915712f5c5c8a87123d1c955ee53f8fd8832ba97
Artem Erusov <erusovav@gmail.com>
2025-06-24 15:58:37 +0300

    Installed plugins and make webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.[contenthash].js',
+      clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
+  };
+};
