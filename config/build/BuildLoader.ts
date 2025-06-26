14685b77154280c792f096d93eab32342c7e9894
Artem Erusov <erusovav@gmail.com>
2025-06-26 19:00:25 +0300

    Fixed a bug with displaying style modules

--- config/build/BuildLoader.ts
+++ config/build/BuildLoader.ts
@@ -2,16 +2,42 @@ import type { ModuleOptions } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import type { BuildOptions } from './types';
 
-export function buildLoaders(_options: BuildOptions): ModuleOptions['rules'] {
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
+  const { isDev } = options;
+
   return [
+    // CSS Modules
+    {
+      test: /\.module\.s[ac]ss$/i,
+      use: [
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: {
+              localIdentName: isDev 
+                ? '[name]__[local]--[hash:base64:5]' 
+                : '[hash:base64:8]',
+              exportLocalsConvention: 'camelCase',
+            },
+            esModule: false,
+            importLoaders: 1,
+          },
+        },
+        'sass-loader',
+      ],
+    },
+    // Global SCSS
     {
       test: /\.s[ac]ss$/i,
+      exclude: /\.module\.s[ac]ss$/i,
       use: [
-        MiniCssExtractPlugin.loader,
+        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
         'css-loader',
         'sass-loader',
       ],
     },
+    // TS
     {
       test: /\.tsx?$/,
       use: 'ts-loader',
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
-import style from './App.module.scss';
+import styles from './App.module.scss';
 
 export const App = () => {
+  console.log(styles);
   return (
-    <button className={style['hello']}>
+    <button className={styles['hello']}>
       <span>Hello world!111222!</span>
     </button>
   );
