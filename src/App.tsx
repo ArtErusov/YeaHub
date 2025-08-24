a38158f90c301ab8e3b0ea9523cdca18e8d4c487
Artem Erusov <erusovav@gmail.com>
2025-08-24 18:25:49 +0300

    Made a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import Test from '@features/d22/d33/Test';
-
 function App() {
-   return <Test />;
+   return <>Ghbdtn</>;
 }
 
 export default App;
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,70 @@
+import { ButtonHTMLAttributes, AnchorHTMLAttributes, ReactNode, forwardRef } from 'react';
+import styles from './Button.module.css';
+
+// Константы для вариантов и размеров кнопки
+export const BUTTON_VARIANTS = ['primary', 'secondary', 'link'] as const;
+export const BUTTON_SIZES = ['S', 'M', 'L'] as const;
+
+// Типы для вариантов и размеров
+export type ButtonVariant = (typeof BUTTON_VARIANTS)[number];
+export type ButtonSize = (typeof BUTTON_SIZES)[number];
+
+// Типизация пропсов кнопки
+export interface BaseButtonProps {
+   variant?: ButtonVariant;
+   size?: ButtonSize;
+   fullWidth?: boolean;
+   destructive?: boolean;
+   preffix?: ReactNode;
+   suffix?: ReactNode;
+   badge?: ReactNode;
+}
+
+type ButtonProps = BaseButtonProps &
+   (ButtonHTMLAttributes<HTMLButtonElement> | AnchorHTMLAttributes<HTMLAnchorElement>);
+
+export const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
+   (
+      {
+         variant = 'primary',
+         size = 'M',
+         fullWidth = false,
+         destructive = false,
+         preffix,
+         suffix,
+         badge,
+         children,
+         className,
+         ...props
+      },
+      ref,
+   ) => {
+      // Определяем, какой HTML-элемент использовать
+      const tagName = props.href ? 'a' : 'button';
+      const Component = tagName as React.ElementType;
+
+      return (
+         <Component
+            ref={ref}
+            className={classNames(
+               styles.button,
+               styles[`button-${variant}`],
+               styles[`button-${size}`],
+               { [styles['button-full']]: fullWidth },
+               { [styles['button-destructive']]: destructive },
+               className,
+            )}
+            {...props}
+         >
+            {preffix}
+            {children}
+            {suffix}
+            {badge && badge !== '0' ? (
+               <span className={styles['button-badge']}>{badge}</span>
+            ) : null}
+         </Component>
+      );
+   },
+);
+
+Button.displayName = 'Button';
