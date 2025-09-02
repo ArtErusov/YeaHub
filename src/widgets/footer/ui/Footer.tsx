018abe67a7864add30cfa8ad214f13045ea5cd4e
Artem Erusov <erusovav@gmail.com>
2025-09-02 18:42:04 +0300

    Reworked the Header component

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,20 +1,21 @@
 import styles from './Footer.module.css';
+import logoLight from '/images/logo-light.svg';
 
 function Footer() {
    return (
       <footer className={styles.container}>
          <div className={styles.block}>
-            <img className={styles.logo} alt="logo" />
-            <p className={styles.title}>Выбери, каким будет IT завтра, вместе с нами</p>
+            <img src={logoLight} className={styles.logo} alt="logo" />
+            <p className={styles.title}>Выбери, каким будет IT завтра, вместе с нами</p>
          </div>
          <p className={styles.description}>
-            YeaHub — это полностью открытый проект, призванный объединить и улучшить IT-сферу.
-            Наш исходный код доступен для просмотра на GitHub. Дизайн проекта также открыт
-            для ознакомления в Figma.
+            YeaHub — это полностью открытый проект, призванный объединить и улучшить IT-сферу. Наш
+            исходный код доступен для просмотра на GitHub. Дизайн проекта также открыт для
+            ознакомления в Figma.
          </p>
          <hr className={styles.separator} />
          <div className={styles.meta}>
-            <p>© 2024 YeaHub</p>
+            <p>© 2024 YeaHub</p>
             <a>Документы</a>
          </div>
       </footer>
--- src/widgets/header/ui/Header/Header.tsx
+++ src/widgets/header/ui/Header/Header.tsx
@@ -1,13 +1,23 @@
-import Nav from '../Nav/Nav';
-import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.scss';
 import { Button } from '@/shared/ui/Button/Button';
+import logo from '/images/logo.svg';
 
 function Header() {
    return (
-      <header className={styles.container}>
-         <img alt="logo" />
-         <Nav />
-         <div>
+      <header className={styles['header']}>
+         <Link to="/" className={styles['header__logo']}>
+            <img src={logo} alt="Логотип сайта" />
+         </Link>
+         <nav className={styles['header__nav']}>
+            <Link className={styles['header__link']} to="#">
+               База вопросов
+            </Link>
+            <Link className={styles['header__link']} to="#">
+               Тренажер
+            </Link>
+         </nav>
+         <div className={styles['header__actions']}>
             <Button variant="secondary">Вход</Button>
             <Button>Регистрация</Button>
          </div>
