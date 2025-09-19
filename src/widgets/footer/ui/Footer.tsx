1a2de0551f1888d68f56336f164c262465760414
Artem Erusov <erusovav@gmail.com>
2025-09-19 14:11:28 +0300

    Improved the footer

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,22 +1,22 @@
-import styles from './Footer.module.css';
+import styles from './Footer.module.scss';
 import logoLight from '/svg/logo-light.svg';
 
 function Footer() {
    return (
-      <footer className={styles.container}>
-         <div className={styles.block}>
-            <img src={logoLight} className={styles.logo} alt="logo" />
-            <p className={styles.title}>Выбери, каким будет IT завтра, вместе с нами</p>
+      <footer className={styles['footer']}>
+         <div className={styles['footer__block']}>
+            <img src={logoLight} className={styles['footer__logo']} alt="logo" />
+            <p className={styles['footer__title']}>Выбери, каким будет IT завтра, вместе с нами</p>
          </div>
-         <p className={styles.description}>
+         <p className={styles['footer__description']}>
             YeaHub — это полностью открытый проект, призванный объединить и улучшить IT-сферу. Наш
             исходный код доступен для просмотра на GitHub. Дизайн проекта также открыт для
             ознакомления в Figma.
          </p>
-         <hr className={styles.separator} />
-         <div className={styles.meta}>
-            <p>© 2024 YeaHub</p>
-            <a>Документы</a>
+         <hr className={styles['footer__separator']} />
+         <div className={styles['footer__meta']}>
+            <p className={styles['footer__copyright']}>© 2024 YeaHub</p>
+            <a className={styles['footer__link']}>Документы</a>
          </div>
       </footer>
    );
