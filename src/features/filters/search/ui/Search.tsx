3e0b71821fcf2d08c0d43808747738adf839f842
Artem Erusov <erusovav@gmail.com>
2025-09-22 11:09:44 +0300

    Added the UseDebounce hook

--- src/features/filters/search/ui/Search.tsx
+++ src/features/filters/search/ui/Search.tsx
@@ -1,30 +1,37 @@
+import { useState, useEffect, type ChangeEvent } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import styles from './Search.module.scss';
+import { useDebounce } from '@/shared/lib/hooks/useDebounce';
 
 function Search() {
    const [searchParams, setSearchParams] = useSearchParams();
-   const title = searchParams.get('title') || '';
+   const title: string = searchParams.get('title') || '';
+   const [inputValue, setInputValue] = useState<string>(title);
 
-   function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
-      const value = e.target.value;
+   const debouncedValue: string = useDebounce(inputValue, 1000);
 
+   useEffect(() => {
       const newParams = new URLSearchParams(searchParams);
 
-      if (value) {
-         newParams.set('title', value);
+      if (debouncedValue) {
+         newParams.set('title', debouncedValue);
          newParams.set('page', '1');
       } else {
          newParams.delete('title');
       }
 
       setSearchParams(newParams);
+   }, [debouncedValue, searchParams, setSearchParams]);
+
+   function handleChangeInput(e: ChangeEvent<HTMLInputElement>) {
+      setInputValue(e.target.value);
    }
 
    return (
       <input
          onChange={handleChangeInput}
          className={styles.input}
-         value={title}
+         value={inputValue}
          type="search"
          placeholder="Введите запрос..."
       />
--- src/widgets/questionDetails/ui/LongAnswer/LongAnswer.tsx
+++ src/widgets/questionDetails/ui/LongAnswer/LongAnswer.tsx
@@ -4,7 +4,7 @@ import styles from './LongAnswer.module.scss';
 function LongAnswer({ longAnswer }: LongAnswerProps) {
    return (
       <div className={styles['long-answer']}>
-         <h3 className={styles['long-answer__title']}>Развёрнутый ответ</h3>
+         <h3 className={styles['long-answer__title']}>Полный ответ</h3>
          <p
             className={styles['long-answer__description']}
             dangerouslySetInnerHTML={{ __html: longAnswer }}
